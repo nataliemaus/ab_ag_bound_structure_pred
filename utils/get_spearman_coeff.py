607ad1f592d24bcca02fa02bfcbfbddfcfa5c639
nataliemaus <natalietmaus@gmail.com>
2024-10-16 13:10:31 -0400

    wip

--- utils/get_spearman_coeff.py
+++ utils/get_spearman_coeff.py
@@ -3,4 +3,11 @@ from scipy import stats
 
 def get_spearman_r(x,y):
     res = stats.spearmanr(x, y)
-    return res.statistic
+    try:
+        r_val = res.statistic.item()
+    except:
+        try:
+            r_val = res.statistic # is item wrong? 
+        except:
+            r_val = res.correlation # older v of scipy 
+    return r_val 
